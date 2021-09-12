24156bc9df814d9385b03531b6ad13fc3081e50a
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2021-09-12 20:04:53 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,20 +1,29 @@
 #bin/#!/bin/bash
 
-	git clone -q --depth=1 https://github.com/mvaisakh/gcc-arm64 -b  gcc-master $HOME/gcc-arm64
-	git clone -q --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master $HOME/gcc-arm32
-	git clone -q --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang $HOME/clang
-	git clone -q --depth=1 https://github.com/Divyanshu-Modi/AnyKernel3 $HOME/Repack
-	git clone -q --depth=1 https://github.com/Atom-X-Devs/android_kernel_xiaomi_sdm660 -b temp $HOME/Kernel
+#	git clone -q --depth=1 https://github.com/mvaisakh/gcc-arm64 -b  gcc-master $HOME/gcc-arm64
+#	git clone -q --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-master $HOME/gcc-arm32
+#	git clone -q --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang $HOME/clang
+#	git clone -q --depth=1 https://github.com/Divyanshu-Modi/AnyKernel3 $HOME/Repack
+#	git clone -q --depth=1 https://github.com/Atom-X-Devs/android_kernel_xiaomi_sdm660 -b temp $HOME/Kernel
 	pip3 -q install telegram-send
 
-	mkdir $HOME/.config
-	mv telegram-send.conf $HOME/.config/telegram-send.conf
-	sed -i s/demo1/${BOT_API_KEY}/g $HOME/.config/telegram-send.conf
-	sed -i s/demo2/${CHAT_ID}/g $HOME/.config/telegram-send.conf
-	mv build.sh $HOME/Kernel/build.sh
+git clone https://github.com/RahifM/android_kernel_xiaomi_msm8953/ -b staging/LA.UM.8.6.r1 --depth 1 $HOME/mido
+git clone https://github.com/AOSPA/android_prebuilts_gcc_linux-x86_arm_arm-eabi -b master --depth 1 $HOME/android/gcc-arm32
+git clone https://github.com/AOSPA/android_prebuilts_gcc_linux-x86_aarch64_aarch64-elf -b master --depth 1 $HOME/android/gcc-arm64
 
-	cd $HOME/Kernel
-	bash build.sh CLANG
+git clone https://github.com/fabianonline/telegram.sh $HOME
+#wget https://raw.githubusercontent.com/RahifM/local_manifests/tgtoken/.telegram.sh 
+#bash ~/tele*/tele* "Tgsh integration test - username: $(whoami)"
+
+
+#	mkdir $HOME/.config
+	#mv telegram-send.conf $HOME/.config/telegram-send.conf
+	#sed -i s/demo1/${BOT_API_KEY}/g $HOME/.config/telegram-send.conf
+	#sed -i s/demo2/${CHAT_ID}/g $HOME/.config/telegram-send.conf
+	mv build.sh $HOME/mido/build.sh
+
+	cd $HOME/mido
+	bash build.sh
 #	bash build.sh GCC
 
 	exit
