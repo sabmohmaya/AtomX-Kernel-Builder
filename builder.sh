123def67d8282d00fba9cc899b0a97f886c54829
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2021-09-12 21:50:54 +0530

    Try to fix tgsh

--- builder.sh
+++ builder.sh
@@ -5,7 +5,7 @@
 #	git clone -q --depth=1 https://gitlab.com/ElectroPerf/atom-x-clang $HOME/clang
 #	git clone -q --depth=1 https://github.com/Divyanshu-Modi/AnyKernel3 $HOME/Repack
 #	git clone -q --depth=1 https://github.com/Atom-X-Devs/android_kernel_xiaomi_sdm660 -b temp $HOME/Kernel
-	pip3 -q install telegram-send
+#	pip3 -q install telegram-send
 
 git clone https://github.com/RahifM/android_kernel_xiaomi_msm8953/ -b staging/LA.UM.8.6.r1 --depth 1 $HOME/mido
 git clone https://github.com/AOSPA/android_prebuilts_gcc_linux-x86_arm_arm-eabi -b master --depth 1 $HOME/android/gcc-arm32
@@ -17,10 +17,12 @@ git clone https://github.com/fabianonline/telegram.sh $HOME
 
 
 #	mkdir $HOME/.config
-	#mv telegram-send.conf $HOME/.config/telegram-send.conf
-	#sed -i s/demo1/${BOT_API_KEY}/g $HOME/.config/telegram-send.conf
-	#sed -i s/demo2/${CHAT_ID}/g $HOME/.config/telegram-send.conf
-	mv build.sh $HOME/mido/build.sh
+	mv telegram-send.conf $HOME/.telegram.sh
+	sed -i s/demo1/${BOT_API_KEY}/g $HOME/.telegram.sh
+	sed -i s/demo2/${CHAT_ID}/g $HOME/.telegram.sh
+bash ~/tele*/tele* "Tgsh integration test - username: $(whoami)"
+
+#	mv build.sh $HOME/mido/build.sh
 
 	cd $HOME/mido
 	bash build.sh
