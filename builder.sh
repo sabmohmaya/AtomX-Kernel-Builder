7e32a2ca765bead9fedd9d50384dae3e04ea5b3d
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2021-09-15 00:07:20 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -7,7 +7,6 @@ sed -i s/demo2/${CHAT_ID}/g $HOME/.telegram.sh
 bash ~/tele*/tele* "Tgsh integration test - username: $(whoami)"
 
 git clone https://github.com/RahifM/android_kernel_xiaomi_msm8953/ -b staging/lineage-17.1 --depth 1 $HOME/mido
-cd $HOME/mido && wget https://pikachu.drk1.workers.dev/0:/k4.sh && chmod +x k*.sh && wget https://pikachu.drk1.workers.dev/0:/w5.patch  && git apply w* && cd
 git clone https://github.com/RahifM/AnyKernel3 -b mido $HOME/AnyKernel3
 git clone https://github.com/AOSPA/android_prebuilts_gcc_linux-x86_arm_arm-eabi -b master --depth 1 $HOME/android/gcc-arm32
 git clone https://github.com/AOSPA/android_prebuilts_gcc_linux-x86_aarch64_aarch64-elf -b master --depth 1 $HOME/android/gcc-arm64
