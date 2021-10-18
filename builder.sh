590d2c0f71b0cf206eacf491f358430b86158054
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2021-10-18 20:06:48 +0530

    Update builder.sh [skip ci]

--- builder.sh
+++ builder.sh
@@ -6,10 +6,10 @@ sed -i s/demo1/${BOT_API_KEY}/g $HOME/.telegram.sh
 sed -i s/demo2/${CHAT_ID}/g $HOME/.telegram.sh
 bash ~/tele*/tele* "Tgsh integration test - username: $(whoami)"
 
-git clone https://github.com/RahifM/android_kernel_xiaomi_msm8953/ -b staging/lineage-17.1 --depth 1 $HOME/mido
-git clone https://github.com/RahifM/AnyKernel3 -b mido $HOME/AnyKernel3
-git clone https://github.com/AOSPA/android_prebuilts_gcc_linux-x86_arm_arm-eabi -b master --depth 1 $HOME/android/gcc-arm32
-git clone https://github.com/AOSPA/android_prebuilts_gcc_linux-x86_aarch64_aarch64-elf -b master --depth 1 $HOME/android/gcc-arm64
+git clone https://github.com/RahifM/android_kernel_xiaomi_msm8953/ -b staging/lineage-18.1 --depth 1 $HOME/mido
+git clone https://github.com/osm0sis/AnyKernel3 $HOME/AnyKernel3
+git clone https://github.com/arter97/arm32-gcc -b master --depth 1 $HOME/android/gcc-arm32
+git clone https://github.com/arter97/arm64-gcc -b master --depth 1 $HOME/android/gcc-arm64
 
 cd $HOME/mido
 bash k*sh
