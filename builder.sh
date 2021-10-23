d4b09e52086a2020a2a50fb22dc17f14ab954aed
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2021-10-23 17:36:14 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -6,7 +6,7 @@ sed -i s/demo1/${BOT_API_KEY}/g $HOME/.telegram.sh
 sed -i s/demo2/${CHAT_ID}/g $HOME/.telegram.sh
 bash ~/tele*/tele* "Tgsh integration test - username: $(whoami)"
 
-git clone https://github.com/RahifM/android_kernel_xiaomi_msm8953/ -b staging/lineage-18.1 --depth 1 $HOME/mido
+git clone https://github.com/RahifM/android_kernel_xiaomi_msm8953/ -b staging/lineage-18.1-clang --depth 1 $HOME/mido
 git clone https://github.com/osm0sis/AnyKernel3 $HOME/AnyKernel3
 git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android-11.0.0_r48 --depth 1 $HOME/clang
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 --depth 1 $HOME/arm
