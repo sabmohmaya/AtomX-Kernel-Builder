08e46bb7441793bf43d9c180144eefaa2ce723d1
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2021-10-26 13:55:10 +0530

    Update clang link

--- builder.sh
+++ builder.sh
@@ -8,7 +8,7 @@ bash ~/tele*/tele* "Tgsh integration test - username: $(whoami)"
 
 git clone https://github.com/RahifM/android_kernel_xiaomi_msm8953/ -b staging/lineage-18.1-clang --depth 1 $HOME/mido
 git clone https://github.com/osm0sis/AnyKernel3 $HOME/AnyKernel3
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android-11.0.0_r48 --depth 1 $HOME/clang
+git clone https://gitlab.com/RahifM/clang -b master --depth 1 $HOME/clang
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 --depth 1 $HOME/arm
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 --depth 1 $HOME/aarch64
 
